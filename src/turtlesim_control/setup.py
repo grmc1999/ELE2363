2736aa52ecf7ce4b1e9c10c80b57ea46b52cbb09
guillermo <gmorenoc@uni.pe>
2024-09-21 17:27:19 -0300

    dbss

--- src/turtlesim_control/setup.py
+++ src/turtlesim_control/setup.py
@@ -11,6 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['launch/turtle_control_launch.py']),
+        ('share/' + package_name, ['launch/turtle_control_line_launch.py']),
         ('share/' + package_name, ['config/params.yaml']),
     ],
     install_requires=['setuptools'],

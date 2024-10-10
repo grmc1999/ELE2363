02f6ee0a9ec85e31a4088f5a43150b40dae39cdf
guillermo <gmorenoc@uni.pe>
2024-10-10 15:35:58 -0300

    gz

---
+++ src/gpg_urdf/launch/gpg_sim.launch.py
@@ -0,0 +1,164 @@
+# Copyright 2020 ros2_control Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.actions import ExecuteProcess
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # Get URDF
+    urdf = os.path.join(get_package_share_directory('gpg_remote'), 'gopigo3.urdf')
+    robot_urdf_model = os.path.join(get_package_share_directory('gpg_remote'), 'gpg.urdf.xml')
+    with open(urdf, 'r') as infp:
+      robot_description_content = infp.read()
+    robot_description = {"robot_description": robot_description_content}
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    with open(robot_urdf_model, 'r') as infp:
+        robot_desc = infp.read()
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("gpg_remote"),
+            "controllers.yaml",
+        ]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("gpg_remote"), "gpg_remote.rviz"]
+    )
+    
+    #robot_publisher = ExecuteProcess(cmd=['ros2', 'topic', 'pub', '-1', '--keep-alive', '86400', '--qos-durability', 'transient_local', '/robot_description', 'std_msgs/String', 'data: \'' + robot_description_content + '\''])
+    robot_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='both',
+            arguments=[robot_urdf_model],
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+    )
+    
+    camera_config = os.path.join(
+      get_package_share_directory('gpg_remote'),
+      'camera.yaml'
+      )
+    image_publisher_node = Node(
+        package="gpg_remote",
+        executable="image_publisher",
+        parameters=[camera_config],
+        output="both",
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    gpg_remote_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["gpg_remote_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    servo_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["servo_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", "-c", "/controller_manager"],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[
+                robot_controller_spawner,
+                servo_controller_spawner,
+                gpg_remote_broadcaster_spawner
+                ],
+        )
+    )
+
+
+    LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('ros_gz_sim'),
+                    'launch',
+                    'gz_sim.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'turtlesim_ns': 'turtlesim2',
+                'use_provided_red': 'True',
+                'new_background_r': TextSubstitution(text=str(colors['background_r']))
+            }.items()
+        )
+    ])
+
+    nodes = [
+        robot_publisher,
+        #control_node,
+        #image_publisher_node,
+        joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+    ]
+
+    return LaunchDescription(nodes)

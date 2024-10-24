5aa7c383eae85647ee5398f6968f76448feface5
guillermo <gmorenoc@uni.pe>
2024-10-24 15:24:53 -0300

    cv node

--- src/gpg_urdf/gpg_urdf/cv_node.py
+++ src/gpg_urdf/gpg_urdf/cv_node.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 import image_geometry
 from cv_bridge import CvBridge
 import cv2
@@ -18,6 +19,7 @@ class cv_node(Node):
         self.declare_parameter('hth',0.01)
         self.cv_bridge=CvBridge()
         self.cv_subscription=self.create_subscription(Image,"/image",self.callback,10)
+        self.ci_subscription=self.create_subscription(CameraInfo,"/camera_info",self.callback_camera_info,10)
         #self.publisher_ = self.create_publisher(Image, '/processed_image', 10)
         self.model=image_geometry.PinholeCameraModel()
         
@@ -34,7 +36,7 @@ class cv_node(Node):
         self.hth=float(self.get_parameter('hth').get_parameter_value().double_value)
         cv_image=self.cv_bridge.imgmsg_to_cv2(msg)
         
-        self.model.fromCameraInfo(msg)
+        
 
         # image processing
         (u,v)=self.image_process_function(cv_image)
@@ -42,6 +44,10 @@ class cv_node(Node):
         line=self.model.projectPixelTo3dRay((u,v))
         print(line)
         #msg=self.bridge.cv2_to_imgmsg(self.cv_image)
+    
+    def callback_camera_info(self,msg):
+        self.model.fromCameraInfo(msg)
+        
 
 
 

d2a72770398ab6e46396a0028776f7f95bfa73e8
guillermo <gmorenoc@uni.pe>
2024-10-31 13:20:00 -0300

    new

--- src/gpg_urdf/gpg_urdf/cv_node.py
+++ src/gpg_urdf/gpg_urdf/cv_node.py
@@ -68,8 +68,8 @@ class cv_node(Node):
         #Minimize 2
         self.servo_pos=self.servo_pos-line[1]
         FP=Float64MultiArray()
-        FP.data=self.servo_pos
-        self.camera_publisher.publish([FP])
+        FP.data=[self.servo_pos]
+        self.camera_publisher.publish(FP)
 
         #msg=self.bridge.cv2_to_imgmsg(self.cv_image)
     

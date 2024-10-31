2064b7cf1b70ff6b7b59f0a146aa42936357f97e
guillermo <gmorenoc@uni.pe>
2024-10-31 14:59:36 -0300

    new

--- src/gpg_urdf/gpg_urdf/cv_node.py
+++ src/gpg_urdf/gpg_urdf/cv_node.py
@@ -96,8 +96,8 @@ class cv_node(Node):
         print(np.arcsin(line[1]))
         print(np.arcsin(line[2]))
         #Minimize 2
-        if self.servo_pos<1.5 and self.servo_pos>-1.5:
-            self.servo_pos=self.servo_pos-line[1]*self.C_k
+        #if self.servo_pos<1.5 and self.servo_pos>-1.5:
+        self.servo_pos=self.servo_pos-line[1]*self.C_k
         FP=Float64MultiArray()
         FP.data=[self.servo_pos]
         self.camera_publisher.publish(FP)

3a1ba52f4bbc8c0b41fd078318c3d77d99b0067a
guillermo <gmorenoc@uni.pe>
2024-10-31 17:19:28 -0300

    new

--- src/gpg_urdf/setup.py
+++ src/gpg_urdf/setup.py
@@ -11,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['launch/gpg_sim.launch.py']),
+        ('share/' + package_name, ['launch/gpg_sim_futebol.launch.py']),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['config/world_1.sdf']),
         ('share/' + package_name, ['config/camera.yaml']),
